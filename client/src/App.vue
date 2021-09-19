e2025d90f91747c1b70d9da74e608dd9b8ca9be0
Ramis Şeker <ramis.seker@gmail.com>
2021-09-19 21:30:55 +0300

    deploy

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" class="is-max-desktop p-5">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+@import '~bulma/css/bulma.css';
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import Index from './components/CategoryList.vue';
+// import Index2 from './components/CategoryList2.vue';
+
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const routes = [
+  {
+    name: 'Index',
+    path: '/',
+    component: Index,
+  },
+  // {
+  //   name: 'Index2',
+  //   path: '/Index2',
+  //   component: Index2,
+  // },
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes });
+
+new Vue({
+  // init router
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
